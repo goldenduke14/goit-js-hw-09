7bc5fea0239f67eda169e43183e25d8c15ab0c85
goldenduke14 <114021880+goldenduke14@users.noreply.github.com>
2023-03-23 10:21:29 +0000

    Deploying to gh-pages from @ goldenduke14/goit-js-hw-09@763943bb307613129a62b458802c9c6a79ab6687 🚀

---
+++ 01-color-switcher.1582b2d4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d;function o(){a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{t.disabled=!0,d=setInterval(o,1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.1582b2d4.js.map
---
+++ 01-color-switcher.73c5a343.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=document.body;function o(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(o,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.73c5a343.js.map
